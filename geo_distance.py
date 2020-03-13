2ca0f45fb8e1de81c1b7f390b8a4ef108954894d
sanmaisnam <59654489+Sanmaisnam@users.noreply.github.com>
2020-03-13 22:14:42 +0100

    Add files via upload

---
+++ geo_distance.py
@@ -0,0 +1,44 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import distance
+
+import certifi
+import urllib
+import ssl
+
+#here, we pass the certifi certificates into the urlopen method to avoid errors of no certifications
+def uo(args, **kwargs):
+    # We create an ssl context to fetch the certificate provided by the local server without performing validation of
+    # the certificate
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    return urllib.request.urlopen(args, context=ssl_context)
+
+geolocator = Nominatim(user_agent="Golf Caddie App")
+print(uo)
+geolocator.urlopen = uo
+
+location1 = geolocator.geocode(input("Enter your current location address (Where is the ball?):"))
+print("Point A: ", location1.address)
+print("latitude is :-" ,location1.latitude,"\nlongtitude is:-" ,location1.longitude,"\n")
+location1_coord = location1.latitude, location1.longitude
+
+location2 = geolocator.geocode(input("Enter your destination address (Where is the hole?): "))
+print("Point B:", location2.address)
+print("latitude is :-" ,location2.latitude,"\nlongtitude is:-" ,location2.longitude,"\n")
+location2_coord = location2.latitude, location2.longitude
+
+# Uses Vincenty’s formulae by default
+d = distance(location1_coord, location2_coord)
+
+print("Your distance is: ", d)
+
+
+'''
+Refs:
+1. https://stackoverflow.com/questions/25888396/how-to-get-latitude-longitude-with-python
+2. https://janakiev.com/blog/gps-points-distance-python/ 
+3. https://pypi.org/project/geopy/ 
+4. https://github.com/geopy/geopy/issues/124
+5. urllib.request — Extensible library for opening URLs (Documentation):
+    https://docs.python.org/3/library/urllib.request.html 
+6. https://bugs.python.org/issue28022 
+'''
